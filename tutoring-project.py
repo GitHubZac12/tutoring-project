5fbf043cb411eba1dcdb1d1368fe23ce5b4719dc
David-Bonck <david.m.bonck@email.com>
2025-02-20 14:26:21 -0600

    "test2"

--- tutoring-project.py
+++ tutoring-project.py
@@ -7,3 +7,7 @@ driver = webdriver.Chrome()
 driver.get("https://docs.google.com/spreadsheets/d/1PbOt5R7eOe_4FNOogczsUG_TfCy8zg6FY34NfFy37T8/edit?gid=0#gid=0")
 
 ActionChains(driver).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys(Keys.ARROW_DOWN).key_up(Keys.CONTROL).key_up(Keys.SHIFT).perform()
+
+df = pd.read_clipboard()
+
+print (df["Student Id"].uique())
