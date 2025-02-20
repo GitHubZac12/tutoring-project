ac8bc39a47650be08ff0c8805d59730a31198bda
David-Bonck <david.m.bonck@email.com>
2025-02-20 14:54:31 -0600

    changes32

--- tutoring-project.py
+++ tutoring-project.py
@@ -6,8 +6,8 @@ import pandas as pd
 driver = webdriver.Chrome()
 driver.get("https://docs.google.com/spreadsheets/d/1PbOt5R7eOe_4FNOogczsUG_TfCy8zg6FY34NfFy37T8/edit?gid=0#gid=0")
 
-ActionChains(driver).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys(Keys.ARROW_DOWN).key_up(Keys.CONTROL).key_up(Keys.SHIFT).perform()
+ActionChains(driver).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_RIGHT).key_up(Keys.SHIFT).send_keys("c").perform()
 
 df = pd.read_clipboard()
 
-print (df["Student Id"].unique())
+print (df)
